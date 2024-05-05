138aaa7ed68c3c2587b51f0610499df309637d32
Arexon Mortel <96506522+arexonmortel@users.noreply.github.com>
2024-05-05 09:29:20 +0800

    ignore config.js

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,7 @@ export const PORT = 5555;
 export const MONGO_URL = 'mongodb+srv://root:root@book-store-mern.kbx9bqv.mongodb.net/books-collection?retryWrites=true&w=majority';
 
 
-export const AWS_ACCESS_KEY_ID='YOUR_ACCESS_KEY'
-export const AWS_SECRET_ACCESS_KEY= 'YOUR_SECRET_ACESS_KEY'
-export const S3_BUCKET_NAME = 'BUCKET_NAME'
-export const S3_BUCKET_REGION = 'BUCKET_REGION'
+export const AWS_ACCESS_KEY_ID='AKIAZI2LGIZR5J5WMKVJ'
+export const AWS_SECRET_ACCESS_KEY= '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'
+export const S3_BUCKET_NAME = 'ggcawsbucket'
+export const S3_BUCKET_REGION = 'ap-southeast-2'
