eae6a1279c7dc90d2024710f7ebf9a0db6310b2f
Arexon Mortel <96506522+arexonmortel@users.noreply.github.com>
2024-05-26 08:54:50 +0800

    "Added react-toastify library and implemented toast notifications for create and delete book functionality, updated create book form with react-select components and ISBN formatting, and refactored delete book page to display toast notification on success."

--- frontend/src/pages/createBook.jsx
+++ frontend/src/pages/createBook.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import BackButton from '../components/home/BackButton';
 import Spinner from '../components/spinner';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import Select from 'react-select'
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function createBook() {
   const [title, setTitle] = useState('');
@@ -25,7 +28,12 @@ function createBook() {
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
-    setImage(file);
+    if (file && file.type.startsWith('image/')) {
+      setImage(file);
+    } else {
+      toast.error('Please select a valid image file.');
+      e.target.value = null; // Clear the input
+    }
     console.log(file);
 
     // Display image preview
@@ -66,13 +74,110 @@ function createBook() {
         }
       );
 
+      setTitle('')
+      setAuthor('')
+      setGenre('')
+      setPublisher('')
+      setApprovedBy('')
+      setEduLevel('')
+      setAvailability('')
+      setPubYear('')
+      setIsbn('')
+      setBookSize('')
+      setPages('')
+      setImage(null)
+      setImagePreview(null)
+
       setLoading(false);
-      navigate('/');
+      toast.success('Book created successfully!', {
+        onClose: () => navigate('/')
+      });
     } catch (error) {
       console.error('Error creating book:', error);
       setLoading(false);
-      setErrorMessage('Error creating book. Please try again.');
+      setErrorMessage('Error creating book. All fields are required.');
+      toast.error('Error creating book. All fields are required.');
     }
+  };
+
+  const handlePubYearChange = (e) => {
+    const currentYear = new Date().getFullYear();
+    let value = e.target.value;
+
+    // Ensure the entered year is not greater than the current year
+    if (value > currentYear) {
+      value = currentYear;
+    }
+
+    setPubYear(value);
+  };
+  
+  const optionsPublisher = [
+    { value: 'Merryland', label: 'Merryland' },
+    { value: 'Jedigar', label: 'Jedigar' },
+    { value: 'B2g2', label: 'B2g2' },
+  ];
+  const optionsApproved = [
+    { value: 'DepEd', label: 'DepEd' },
+    { value: 'For Approval', label: 'For Approval' },
+  ];
+  const optionsBookSize = [
+    { value: '7" x 10"', label: 'Small - 7" x 10' },
+    { value: '12" x 18"', label: 'Big - 12" x 18' },
+  ];
+  const optionsGenre = [
+    { value: 'Storybook', label: 'Storybook' },
+    { value: 'Science Modules', label: 'Science Modules' },
+    { value: 'Workbook', label: 'Workbook' },
+    { value: 'Math Modules', label: 'Math Modules' },
+    { value: 'Pre Elementary', label: 'Pre Elementary' },
+  ];
+
+  const customStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      border: '2px solid #e2e8f0',
+      borderRadius: '0.375rem',
+      text: 'rgb(107 114 128 / var(--tw-placeholder-opacity))',
+      boxShadow: state.isFocused ? '0 0 0 1px rgba(25, 24, 71, 0.2)' : 'none',
+      '&:hover': {
+        borderColor: 'rgba(25, 24, 71, 0.2)',
+      },
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isSelected ? '#191847' : 'white',
+      color: state.isSelected ? 'white' : '#4a5568',
+      '&:hover': {
+        backgroundColor: 'rgba(25, 24, 71, 0.2)',
+        color: '#191847',
+      },
+    }),
+  };
+
+  const handleIsbnChange = (e) => {
+    const { value } = e.target;
+    const formattedValue = formatIsbn(value);
+    setIsbn(formattedValue);
+  };
+
+  const formatIsbn = (value) => {
+    // Remove all non-digit characters
+    const digitsOnly = value.replace(/\D/g, '');
+
+    // Limit the input to 13 digits
+    const limitedDigits = digitsOnly.substring(0, 13);
+
+    // Insert dashes at the correct positions
+    let formattedValue = '';
+    for (let i = 0; i < limitedDigits.length; i++) {
+      if (i === 3 || i === 6 || i === 9 || i === 12) {
+        formattedValue += '-';
+      }
+      formattedValue += limitedDigits[i];
+    }
+
+    return formattedValue;
   };
 
   return (
@@ -113,7 +218,7 @@ function createBook() {
               placeholder="Title"
               value={title}
               onChange={(e) => setTitle(e.target.value)}
-              className="p-2 border-2 border-gray-200 rounded-lg "
+              className="p-2 border-2 border-gray-200 rounded-lg placeholder-gray-500 focus:outline-none focus:shadow-outline focus:border-gray-300"
               required
             />
             <input
@@ -121,81 +226,79 @@ function createBook() {
               placeholder="Author"
               value={author}
               onChange={(e) => setAuthor(e.target.value)}
-              className="p-2 border-2 border-gray-200 rounded-lg "
+              className="p-2 border-2 border-gray-200 rounded-lg placeholder-gray-500 focus:outline-none focus:shadow-outline focus:border-gray-300"
               required
             />
-            <input
-              type="text"
-              placeholder="Genre"
-              value={genre}
-              onChange={(e) => setGenre(e.target.value)}
-              className="p-2 border-2 border-gray-200 rounded-lg "
-              required
-            />
-            <input
-              type="text"
-              placeholder="Publisher"
-              value={publisher}
-              onChange={(e) => setPublisher(e.target.value)}
-              className="p-2 border-2 border-gray-200 rounded-lg "
-              required
-            />
-            <input
-              type="text"
-              placeholder="Approved By"
-              value={approvedBy}
-              onChange={(e) => setApprovedBy(e.target.value)}
-              className="p-2 border-2 border-gray-200 rounded-lg "
-              required
-            />
+          <Select
+          className="rounded-lg outline-none"
+          options={optionsGenre}
+          onChange= {(e) => {setGenre(e.value)}}
+          styles={customStyles}
+          placeholder="Genre"
+        />
+        <Select
+          className="rounded-lg outline-none"
+          options={optionsPublisher}
+          onChange= {(e) => {setPublisher(e.value)}}
+          styles={customStyles}
+          placeholder="Publisher"
+        />
+        <Select
+          className="rounded-lg outline-none"
+          options={optionsApproved}
+          onChange= {(e) => {setApprovedBy(e.value)}}
+          styles={customStyles}
+          placeholder="Approved By"
+        />
             <input
               type="text"
               placeholder="Education Level (e.g. Grade 1, Grade 2, etc.)"
               value={eduLevel}
               onChange={(e) => setEduLevel(e.target.value)}
-              className="p-2 border-2 border-gray-200 rounded-lg "
+              className="p-2 border-2 border-gray-200 rounded-lg placeholder-gray-500 focus:outline-none focus:shadow-outline focus:border-gray-300"
               required
             />
             <input
               type="number"
               placeholder="Publication Year"
               value={pubYear}
-              onChange={(e) => setPubYear(e.target.value)}
-              className="p-2 border-2 border-gray-200 rounded-lg "
+              onChange={handlePubYearChange}
+              className="p-2 border-2 border-gray-200 rounded-lg placeholder-gray-500 focus:outline-none focus:shadow-outline focus:border-gray-300"
               required
               min="1900" // Adjust the minimum year as needed
               max={new Date().getFullYear()} // Set the maximum year to the current year
             />
-            <input
-              type="text"
-              placeholder="ISBN"
-              value={isbn}
-              onChange={(e) => setIsbn(e.target.value)}
-              className="p-2 border-2 border-gray-200 rounded-lg "
-              required
-            />
-            <input
-              type="text"
-              placeholder="Book Size"
-              value={bookSize}
-              onChange={(e) => setBookSize(e.target.value)}
-              className="p-2 border-2 border-gray-200 rounded-lg "
-              required
-            />
+    <input
+      type="text"
+      placeholder="ISBN"
+      value={isbn}
+      onChange={handleIsbnChange}
+      className="p-2 border-2 border-gray-200 rounded-lg placeholder-gray-500 focus:outline-none focus:shadow-outline focus:border-gray-300"
+      required
+    />
+        <Select
+          className="rounded-lg outline-none"
+          options={optionsBookSize}
+          onChange= {(e) => {setBookSize(e.value)}}
+          styles={customStyles}
+          placeholder="Book Size"
+        />
             <input
               type="number"
               placeholder="Pages"
               value={pages}
+              min={1}
               onChange={(e) => setPages(e.target.value)}
-              className="p-2 border-2 border-gray-200 rounded-lg "
+              className="p-2 border-2 border-gray-200 rounded-lg placeholder-gray-500 focus:outline-none focus:shadow-outline focus:border-gray-300"
               required
             />
             <input
               type="number"
               placeholder="No. of Copies"
               value={availability}
+              min={0}
               onChange={(e) => setAvailability(e.target.value)}
-              className="p-2 border-2 border-gray-200 rounded-lg "
+              className="p-2 border-2 border-gray-200 rounded-lg placeholder-gray-500 focus:outline-none focus:shadow-outline focus:border-gray-300"
               required
             />
             <button
@@ -208,6 +311,8 @@ function createBook() {
               <p className="text-red-400 text-sm">{errorMessage}</p>
             )}
           </div>
+      <ToastContainer
+      autoClose={3000} />
         </div>
       )}
     </div>
--- frontend/src/pages/deleteBook.jsx
+++ frontend/src/pages/deleteBook.jsx
@@ -2,49 +2,69 @@ import React, {useState, useEffect} from 'react';
 import Spinner from '../components/spinner'
 import axios from 'axios'
 import { useNavigate, useParams } from 'react-router-dom'
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function deleteBook() {
   const navigate = useNavigate()
   const { id } = useParams()
   const [loading, setLoading] = useState(false)
   const [book, setBook] = useState({})
+  const [successDelete, setSuccessDelete] = useState(false)
 
-  const handleDeleteBook = ()=>{
+  const handleDeleteBook = () => {
     setLoading(true);
     axios.delete(`http://localhost:5555/books/${id}`)
-    .then((res)=>{
-      setLoading(false)
-      navigate('/')
-    })
-  }
+      .then((res) => {
+        setLoading(false);
+        showToast();
+        setSuccessDelete(true)
+      })
+      .catch((error) => {
+        setLoading(false);
+        toast.error('Error deleting book. Please try again.');
+      });
+  };
+
+  const showToast = () => {
+    toast.success('Book deleted successfully!', {
+      onClose: () => navigate('/')
+    });
+  };
 
   const handleCancel = ()=>{
     navigate('/');
   }
   return (
-    <div className='p-4'>
-      {loading && <Spinner />}
-      <div className='w-[400px] rounded-xl mx-auto p-6 bg-white shadow-md'>
-        <h1 className='text-3xl font-semibold mb-3 opacity-85'>Delete Book</h1>
-        <div className='flex flex-col gap-y-4'>
-          <p >Are you sure you want to delete this book?</p>
-          <div className='flex justify-between'>
-            <button
-              onClick={handleDeleteBook}
-              className='bg-red-500 text-white px-4 py-2 rounded-lg transition-all duration-300 ease-in-out hover:bg-white hover:text-red-500 hover:ring-inset hover:ring-1 hover:ring-red-500 mr-2'
-            >
-              Delete
-            </button>
-            <button
-              onClick={handleCancel}
-              className='bg-primary-txt opacity-75 text-white  px-4 py-2 rounded-lg transition-all duration-300 ease-in-out hover:bg-white hover:text-primary-txt hover:ring-inset hover:ring-1 hover:ring-gray-500'
-            >
-              Cancel
-            </button>
-          </div>
+<div className='p-4'>
+  {loading ? (
+    <Spinner />
+  ) : successDelete ? (
+    <ToastContainer autoClose = {2000} />
+  ) : (
+    <div className='w-[400px] rounded-xl mx-auto p-6 bg-white shadow-md'>
+      <h1 className='text-3xl font-semibold mb-3 opacity-85'>Delete Book</h1>
+      <div className='flex flex-col gap-y-4'>
+        <p>Are you sure you want to delete this book?</p>
+        <div className='flex justify-between'>
+          <button
+            onClick={handleDeleteBook}
+            className='bg-red-500 text-white px-4 py-2 rounded-lg transition-all duration-300 ease-in-out hover:bg-white hover:text-red-500 hover:ring-inset hover:ring-1 hover:ring-red-500 mr-2'
+          >
+            Delete
+          </button>
+          <button
+            onClick={handleCancel}
+            className='bg-primary-txt opacity-75 text-white px-4 py-2 rounded-lg transition-all duration-300 ease-in-out hover:bg-white hover:text-primary-txt hover:ring-inset hover:ring-1 hover:ring-gray-500'
+          >
+            Cancel
+          </button>
         </div>
       </div>
     </div>
+  )}
+</div>
+
   )
 }
 
